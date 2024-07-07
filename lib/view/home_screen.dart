a4fcb724886bc39e7c4277f48240946ce5ef9304
Hirunika Sathsarani <90099754+HirunikaSathsarani99@users.noreply.github.com>
2024-07-07 15:05:48 +0530

    app_exit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:linkedin_clone_app/model/user.dart';
 import 'package:linkedin_clone_app/view/components/search_bar.dart';
 import 'package:linkedin_clone_app/view/styles.dart';
@@ -30,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               TextButton(
                 onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/main');
+                    SystemNavigator.pop();
                 },
                 child: Text('Yes'),
               ),
